7701170371032d8bcc2da0c986abf421337c3331
atharv7676 <atharvmorabale7@gmail.com>
2026-08-31 22:24:18 +0530

    fix: update movie fetching to use centralized API service

--- movie-mafia-frontend/src/components/Hero.jsx
+++ movie-mafia-frontend/src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Spinner } from "@/components/ui/spinner";
+import api from "@/services/api";
 
 const Hero = () => {
   const [movies, setMovies] = useState([]);
@@ -11,7 +12,7 @@ const Hero = () => {
   useEffect(() => {
     const getMovies = async () => {
       try {
-        const response = await axios.get("http://localhost:3000/movies");
+        const response = await api.get("/movies")
 
         const movieList = response.data.data;
 
